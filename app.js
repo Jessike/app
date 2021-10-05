849baa83d0712bf7b77c5de039f5f3c3324aa3be
Jessike Koort <jessike.koort@khk.ee>
2021-10-05 14:21:32 +0300

    client

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./src/router');
 const app = express();
 const port = 3000;
 const {expressLogger, logger} = require('./src/utils/logger');
 
-
+app.use(cors({
+  origin: 'https://www.section.io'
+}));
 app.use(expressLogger);
 app.use('/api/v1', routes);
 
--- src/routes/post-user.js
+++ src/routes/post-user.js
@@ -4,6 +4,7 @@ const {hashPassword} = require('./../utils/hashing');
 const createUser = async (req, res) => {
   const hash = await hashPassword(req.body.password);
   const email = req.body.email;
+
   try {
     const person = await createUse(email, hash);
     res.status(200).json(person);
